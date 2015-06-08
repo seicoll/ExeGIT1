427550e1686ce76c5528016fe12bd28138f60b3c
Sei Coll <seicoll@gmail.com>
2015-06-08 13:01:40 +0200

    Modifica el programa per contar els números

--- exeGIT.js
+++ exeGIT.js
@@ -2,8 +2,10 @@
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+	var numeros = "1234567890".split("");
     var frase = prompt("Introdueix una frase");
     var numLletres = 0;
+	var numNumeros = 0;
     
     for (i=0; i < frase.length; i++)
     {
@@ -13,8 +15,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
         {
             numLletres++
         }
+		
+		if (numeros.indexOf(frase[i]) != -1)   //Si  es troba el caracter a l'array numeros
+        {
+            numNumeros++
+        }
     }
     
     //document.write("<p>S'han trobat " + numLletres +" lletres en la frase introduïda.</p>")
-    document.getElementsByTagName("p")[0].innerHTML = "S'han trobat " + numLletres +" lletres en la frase introduïda."
+    document.getElementsByTagName("p")[0].innerHTML = "S'han trobat " + numLletres +" lletres i "+ numNumeros+ " números en la frase introduïda."
 });
